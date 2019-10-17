ba8fca7c7afd4720843ce27cda80d69e93aa148c
Petani Kode <muhammadlukman09@gmail.com>
2019-10-17 18:01:27 +0700

    project tigatiga

---
+++ src/pboul/pkg10115229/latihan33/userlogin/User.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan33.userlogin;
+
+/**
+ *
+ * @author Lukman
+ */
+public class User {
+    
+    private String username;
+    private String password;
+    private boolean statusAkun;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isStatusAkun() {
+        return statusAkun;
+    }
+
+    public void setStatusAkun(boolean statusAkun) {
+        this.statusAkun = statusAkun;
+    }
+    
+    
+    
+}
